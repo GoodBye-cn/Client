5d48360fe4bfd28c4efb2141f5410df81075aaa3
Goodbye <guwei9911@outlook.com>
2023-04-09 00:05:25 +0800

    update

--- inc/FTServerClient.h
+++ inc/FTServerClient.h
@@ -1,6 +1,25 @@
 #ifndef FTPSERVERCLIENT_H
 #define FTPSERVERCLIENT_H
 
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+struct Request {
+    int length;
+    char path[256];
+};
+
+struct Response {
+    int length;
+    char end[4];
+};
+
 class FTServerClient {
 public:
     FTServerClient();
--- src/FTServerClient.cc
+++ src/FTServerClient.cc
@@ -1,5 +1,31 @@
 #include "FTServerClient.h"
 
-FTServerClient::FTServerClient() {}
+FTServerClient::FTServerClient() {
+    struct Request req;
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+
+    serv_addr.sin_port = htons(8080);
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_addr.sin_family = AF_INET;
+
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    int res = connect(fd, (sockaddr*)&serv_addr, sizeof(serv_addr));
+    if (res < 0) {
+        printf("connect error\n");
+        exit(1);
+    }
+
+    char buf[512] = { 0 };
+    size_t ret = 0;
+    ret = send(fd, (void*)&req, req.length + sizeof(int), 0);
+    printf("ret: %ld", ret);
+    if (ret < 0) {
+        printf("send error\n");
+        close(fd);
+        exit(0);
+    }
+}
 
 FTServerClient::~FTServerClient() {}
